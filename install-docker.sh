d583422b37639a207d5a25fd2c7a176af3f23680
Peter Reuterås <peter@reuteras.com>
2024-09-07 09:17:44 +0200

    Fix for Kali

--- install-docker.sh
+++ install-docker.sh
@@ -45,9 +45,14 @@ if grep PRETTY_NAME /etc/os-release | grep -E "(Debian|Kali)" > /dev/null ; then
 
 	# Add the repository to Apt sources:
 	# shellcheck disable=SC1091
+    VERSION_CODENAME=$(. /etc/os-release && echo "$VERSION_CODENAME")
+    if [[ "$VERSION_CODENAME" == "kali-roling" ]]; then
+        VERSION_CODENAME="bookworm"
+    fi
+	# shellcheck disable=SC1091
 	echo \
   		"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
-  		$(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  		$VERSION_CODENAME stable" | \
   		sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 	sudo apt-get update
 
